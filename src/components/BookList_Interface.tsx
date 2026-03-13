f45dfb28679d181959666e24d287c1ad68ff445e
mdlaras <52792426+mdlaras@users.noreply.github.com>
2026-03-13 13:26:55 +0100

    input book name and show book list

--- src/components/BookList_Interface.tsx
+++ src/components/BookList_Interface.tsx
@@ -1,5 +1,33 @@
+import { useState } from "react"
+import { books_list } from "../books"
+
 export default function BookList_Interface(){
+    const [books_list,setBooksList] = useState<string[]>([])
+    
+    function add_book(e){
+        e.preventDefault()
+        console.log(e)
+        const formData = new FormData(e.currentTarget)
+        const book_name = formData.get("book_name")
+        console.log(book_name)
+        setBooksList([...books_list, book_name])
+    }
+    
     return(
-        <p>Hello</p>
+        <div>
+            <form onSubmit={add_book}>
+                <label>
+                    Book Name
+                    <input name="book_name" type="text" />
+                </label>
+                <button type="submit">Add</button>
+            </form> 
+            <div>
+                {books_list.map((book,i) => (
+                    <p>{i+1}. {book}</p>
+                ))}
+            </div>
+        </div>
+
     )
 }
